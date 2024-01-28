4efadf3f77102fab1e74375a8b0dbd23f9ab3a27
madhusudhanansuresh <madhusudhanan.suresh@gmail.com>
2024-01-28 13:32:11 -0500

    Added logs

--- ImportDailyIndicators/ImportDailyIndicatorsHandler.cs
+++ ImportDailyIndicators/ImportDailyIndicatorsHandler.cs
@@ -3,6 +3,7 @@ using Microsoft.EntityFrameworkCore;
 using TradeFunctions.Models.Helpers;
 using TradeFunctions.Models.Postgres.TradeContext;
 using TradeFunctions.Services;
+using Microsoft.Extensions.Logging;
 
 namespace TradeFunctions.ImportDailyIndicators
 {
@@ -13,33 +14,40 @@ namespace TradeFunctions.ImportDailyIndicators
 
     public class ImportDailyIndicatorsHandler : IImportDailyIndicatorsHandler
     {
+        private readonly ILogger _logger;
+
         private readonly ITwelveDataService _twelveDataService;
         public IDbConnectionStringService _dbConnectionStringService { get; }
 
-        public ImportDailyIndicatorsHandler(IDbConnectionStringService dbConnectionStringService, ITwelveDataService twelveDataService)
+        public ImportDailyIndicatorsHandler(ILoggerFactory loggerFactory, IDbConnectionStringService dbConnectionStringService, ITwelveDataService twelveDataService)
         {
+            _logger = loggerFactory.CreateLogger<ImportDailyIndicatorsController>();
             _dbConnectionStringService = dbConnectionStringService;
             _twelveDataService = twelveDataService;
         }
 
         public async Task<bool> ImportATR(CancellationToken cancellationToken = default)
         {
+            _logger.LogInformation("Starting ImportATR operation.");
             try
             {
                 var methodContainer = new MethodContainer();
                 methodContainer.AddMethod(new ComplexMethod("ATR", new Dictionary<string, object> { { "time_period", 14 } }));
 
+                _logger.LogInformation("Fetching connection string.");
                 using (var dbContext = new TradeContext(_dbConnectionStringService.ConnectionString()))
                 {
                     var timeFrame = "1day";
+                    _logger.LogInformation("Retrieving tickers.");
                     var tickers = await dbContext.Tickers.Select(x => x.TickerName).Take(1).ToListAsync();
 
+                    _logger.LogInformation($"Fetching stock data for tickers: {string.Join(", ", tickers)}.");
                     var stockDataResponse = await _twelveDataService.FetchStockDataAsync(["AAPL"], [timeFrame], "", "", 1, methodContainer);
 
+                    _logger.LogInformation("Processing stock data response.");
                     var tickerId = await dbContext.Tickers.Where(x => x.TickerName == "AAPL").Select(x => x.Id).FirstOrDefaultAsync();
 
                     dbContext.DailyIndicators.RemoveRange(dbContext.DailyIndicators);
-
                     dbContext.SaveChanges();
 
                     foreach (var stockData in stockDataResponse.Data)
@@ -51,18 +59,17 @@ namespace TradeFunctions.ImportDailyIndicators
                         }
                     }
                     await dbContext.SaveChangesAsync(cancellationToken);
+                    _logger.LogInformation("ImportATR operation completed successfully.");
+                    return true;
                 }
-
-                return true;
             }
             catch (Exception ex)
             {
-
-                Console.WriteLine("An error occurred: " + ex.Message);
-
+                _logger.LogError(ex, "An error occurred during the ImportATR operation.");
                 return false;
             }
         }
+
 
         private DailyIndicator MapToStockPrice(ValueData valueData, MetaData metaData, int tickerId)
         {
