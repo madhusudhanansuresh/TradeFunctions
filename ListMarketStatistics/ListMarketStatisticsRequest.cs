aa90ed344eb44ba347115a235e6e07197ab57176
madhu <madhusudhanan.suresh@gmail.com>
2024-02-24 16:53:43 -0500

    update

--- ListMarketStatistics/ListMarketStatisticsRequest.cs
+++ ListMarketStatistics/ListMarketStatisticsRequest.cs
@@ -1,9 +1,11 @@
 using System;
+using System.Text.Json.Serialization;
 
 namespace TradeFunctions.ListMarketStatistics
 {
     public class ListMarketStatisticsRequest
     {
+         [JsonPropertyName("endDateTime")]
         public string EndDateTime { get; set; }
     }   
 }
